15eff18db81abd29ec059aca1174232310b63daa
kahkurenai <sys1@grupopmz.com.br>
2019-12-09 15:29:28 -0400

    MasterPage

--- src/main/java/pages/MasterPage.java
+++ src/main/java/pages/MasterPage.java
@@ -5,12 +5,18 @@ import org.openqa.selenium.WebDriver;
 public class MasterPage {
 
 	private RestaurantesPage restaurantesPage;
+	private MenuPage menuPage;
 
 	public MasterPage(WebDriver navegador) {
 		this.restaurantesPage = new RestaurantesPage(navegador);
+		this.menuPage = new MenuPage(navegador);
 	}
 
 	public RestaurantesPage getRestaurantesPage() {
 		return restaurantesPage;
+	}
+
+	public MenuPage getMenuPage() {
+		return menuPage;
 	}
 }
